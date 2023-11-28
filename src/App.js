30c8de1ce26c61ce9926e4396a6423b5b164b150
Agnibesh M <agnibesh00mukherjee@gmail.com>
2023-11-28 21:14:29 +0530

    onauthchange added

--- src/App.js
+++ src/App.js
@@ -1,59 +1,80 @@
+import React, { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Register from "./components/Register";
+import Signin from "./components/Signin";
+import UserRegistration from "./components/UserRegistration";
 
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route,Routes, Navigate} from 'react-router-dom';
-import Register from './components/Register';
-import Signin from './components/Signin';
-import UserRegistration from './components/UserRegistration';
+import Explore from "./components/Explore";
+import Profile from "./components/profile";
+import SellBooks from "./components/SellBooks";
 
-import Explore from './components/Explore';
-import Profile from './components/profile';
-import SellBooks from './components/SellBooks';
-
-
-import { supabase } from './supabaseClient';
-
+import { supabase } from "./supabaseClient";
 
 const App = () => {
-const [session,setSession]=useState(null)
+  const [session, setSession] = useState(null);
 
-const getSession=async()=>{
-  const { data, error } = await supabase.auth.getSession()
-  if (data){
-    setSession(data.session);
-  }
-  if(error){
-  console.log(error);
-  }
-}
+  const getSession = async () => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
 
-useEffect(()=> {
-  getSession();
-},[supabase])
-  
-  return(
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  };
+
+  useEffect(() => {
+    getSession();
+  }, [supabase]);
+
+  return (
     <div className="App">
-      <Router>  
-      <Routes>
-      <Route path="/" element={session ? <Navigate replace to={"/Explore"}/>:<Register/>}></Route>
-      <Route path="/Signin" element={session ? <Navigate replace to={"/Explore"}/>:<Signin/>}></Route>
-      <Route path="/UserRegistration" element={session ? <Navigate replace to={"/Explore"}/>:<UserRegistration/>}></Route>
-      
-      <Route path="/Explore" element={<Explore session={session}/>}></Route>
-      <Route path="/Profile" element={<Profile session={session}/>}></Route>
-      <Route path="/SellBooks" element={<SellBooks session={session}/>}></Route>
-      </Routes>       
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              session ? <Navigate replace to={"/Explore"} /> : <Register />
+            }
+          ></Route>
+          <Route
+            path="/Signin"
+            element={
+              session ? <Navigate replace to={"/Explore"} /> : <Signin />
+            }
+          ></Route>
+          <Route
+            path="/UserRegistration"
+            element={
+              session ? (
+                <Navigate replace to={"/Explore"} />
+              ) : (
+                <UserRegistration />
+              )
+            }
+          ></Route>
+
+          <Route
+            path="/Explore"
+            element={<Explore session={session} />}
+          ></Route>
+          <Route
+            path="/Profile"
+            element={<Profile session={session} />}
+          ></Route>
+          <Route
+            path="/SellBooks"
+            element={<SellBooks session={session} />}
+          ></Route>
+        </Routes>
       </Router>
-    
     </div>
- 
   );
 };
 
 export default App;
- 
-  
-
-
-
-
-
